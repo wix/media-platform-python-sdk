811d728841c1b8eb7d785213511bafc9c72dd6f3
elad laufer <elad@wix.com>
2018-11-28 18:11:56 +0200

    - request headers - initial response handler

--- media_platform/http/authenticated_http_client.py
+++ media_platform/http/authenticated_http_client.py
@@ -1,4 +1,6 @@
 import requests
+from requests import Response
+from requests.structures import CaseInsensitiveDict
 
 from media_platform.auth.authenticator import Authenticator
 from media_platform.service.rest_result import RestResult
@@ -17,12 +19,26 @@ class AuthenticatedHTTPClient(object):
     def get(self, url, params=None):
         # type: (str, dict) -> RestResult
 
+        response = requests.get(url, params, headers=self._headers())
+
+        return self._handle_response(response)
+
+    def _headers(self):
+        # type: () -> CaseInsensitiveDict
+
         signed_token = self.authenticator.default_signed_token()
 
-        response = requests.get(url, params, headers={
-            'Authorization': signed_token
-        })
+        headers = requests.utils.default_headers()
+        headers['Authorization'] = signed_token
+        headers['User-Agent'] = self.USER_AGENT
+        headers['Accept'] = 'application/json'
+
+        return headers
+
+    def _handle_response(self, response):
+        # type: (Response) -> RestResult
+
+        # todo: error handling
 
         data = response.json()
-
         return RestResult.deserialize(data)
