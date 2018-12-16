146f6e755f5e03499a269c05ec1abe4b46922235
elad laufer <elad@wix.com>
2018-12-16 16:43:05 +0200

    - exportation

--- media_platform/__init__.py
+++ media_platform/__init__.py
@@ -1,4 +1,11 @@
 from media_platform.media_platform_client import MediaPlatformClient
-
+from media_platform.service.destination import Destination
+from media_platform.service.file_descriptor import FileDescriptor
+from media_platform.service.source import Source
 
 Client = MediaPlatformClient
+
+source = Source
+destination = Destination
+
+file_descriptor = FileDescriptor
