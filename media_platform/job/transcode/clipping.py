0faed0c59d211522b0a4b6f757dab655a22badb0
alonne <alonne@wix.com>
2018-12-27 14:46:52 +0200

    Fixing clipping serialization

--- media_platform/job/transcode/clipping.py
+++ media_platform/job/transcode/clipping.py
@@ -2,8 +2,8 @@ from media_platform.lang.serialization import Serializable, Deserializable
 
 
 class Clipping(Serializable, Deserializable):
-    def __init__(self, start=None, duration=None, fade_in_duration=None, fade_out_duration=None,
-                 fade_in_offset=None, fade_out_offset=None):
+    def __init__(self, start=None, duration=None, fade_in_duration=None, fade_out_duration=None, fade_in_offset=None,
+                 fade_out_offset=None):
         # type: (int, int, int, int, int, int) -> None
         self.start = start
         self.duration = duration
@@ -12,7 +12,14 @@ class Clipping(Serializable, Deserializable):
         self.fade_in_offset = fade_in_offset
         self.fade_out_offset = fade_out_offset
 
+    @classmethod
+    def deserialize(cls, data):
+        # type: (dict) -> Clipping
+        return cls(data.get('start'), data.get('duration'), data.get('fadeInDuration'), data.get('fadeOutDuration'),
+                   data.get('fadeInOffset'), data.get('fadeOutOffset'))
+
     def serialize(self):
+        # type: () -> dict
         return {
             'start': self.start,
             'duration': self.duration,
@@ -21,8 +28,3 @@ class Clipping(Serializable, Deserializable):
             'fadeInOffset': self.fade_in_offset,
             'fadeOutOffset': self.fade_out_offset
         }
-
-    @classmethod
-    def deserialize(cls, data):
-        return cls(data.get('clipStart'), data.get('clipEnd'), data.get('fadeInDuration'), data.get('fadeOutDuration'),
-                   data.get('fadeInOffset'), data.get('fadeOutOffset'))
