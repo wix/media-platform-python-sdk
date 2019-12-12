fc4d5ca8e6301ea2142ec260ee60cb69ebca65ae
alonne <alonne@wix.com>
2019-12-12 11:15:58 +0200

    Add callback to import request

--- media_platform/service/file_service/import_file_request.py
+++ media_platform/service/file_service/import_file_request.py
@@ -1,5 +1,6 @@
 from media_platform.http.authenticated_http_client import AuthenticatedHTTPClient
 from media_platform.job.import_file_job import ImportFileJob
+from media_platform.service.callback import Callback
 from media_platform.service.destination import Destination
 from media_platform.service.file_service.external_authorization import ExternalAuthorization
 from media_platform.service.media_platform_request import MediaPlatformRequest
@@ -14,6 +15,7 @@ class ImportFileRequest(MediaPlatformRequest):
         self.source_url = None
         self.external_authorization = None  # type: ExternalAuthorization
         self.destination = None  # type: Destination
+        self.job_callback = None  # type: Callback
 
     def set_source_url(self, source_url):
         # type: (str) -> ImportFileRequest
@@ -30,6 +32,11 @@ class ImportFileRequest(MediaPlatformRequest):
         self.destination = destination
         return self
 
+    def set_job_callback(self, job_callback):
+        # type: (Callback) -> ImportFileRequest
+        self.job_callback = job_callback
+        return self
+
     def execute(self):
         # type: () -> ImportFileJob
         return super(ImportFileRequest, self).execute()
@@ -38,5 +45,6 @@ class ImportFileRequest(MediaPlatformRequest):
         return {
             'sourceUrl': self.source_url,
             'externalAuthorization': self.external_authorization.serialize() if self.external_authorization else None,
-            'destination': self.destination.serialize()
+            'destination': self.destination.serialize(),
+            'jobCallback': self.job_callback.serialize() if self.job_callback else None
         }
