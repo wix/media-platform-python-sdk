54c6636f96e03783f03e6cc91c7e19b71847013c
alonne <alonne@wix.com>
2020-07-22 13:08:38 +0300

    Try to fix tests

--- tests.sh
+++ tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-virtualenv test-venv
-source test-venv/bin/activate
+python3 -m venv test-run
+source test-run/bin/activate
 
 pip install -e .
 
-python setup.py test || exit 1
+python3 setup.py test || exit 1
